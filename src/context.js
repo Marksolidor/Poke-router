4c30d44254c0524e4e8a4b27c172f77de18552ec
Marksolidor <marco350sl@gmail.com>
2022-10-08 19:03:59 -0300

    fixes

--- src/context.js
+++ src/context.js
@@ -4,17 +4,34 @@ export const Context = createContext();
 
 const ImgProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [info, setInfo] = useState([]);
 
   useEffect(() => {
     const dataConsult = async () => {
+      try{
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon/?limit=2000"
+        `https://pokeapi.co/api/v2/pokemon/?limit=151`
       );
       const dataImg = await response.json();
       setData(dataImg);
+      } catch (err) {}
     };
 
     dataConsult();
+  }, []);
+
+  useEffect(() => {
+    const infoConsult = async () => {
+      try{
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/?limit=151`
+      );
+      const dataImg = await response.json();
+      setData(dataImg);
+      } catch (err) {}
+    };
+
+    infoConsult();
   }, []);
 
   return (
